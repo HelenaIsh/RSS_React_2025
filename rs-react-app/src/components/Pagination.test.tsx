8639b9de90691e72124d5488e596ec99b240411c
HelenaIsh <elena-ishmatova@mail.ru>
2025-02-12 13:58:53 +0100

    fix: fix ts errors

--- rs-react-app/src/components/Pagination.test.tsx
+++ rs-react-app/src/components/Pagination.test.tsx
@@ -6,7 +6,7 @@ import '@testing-library/jest-dom';
 import { useSearchParams } from 'react-router-dom';
 
 vi.mock('react-router-dom', async (importOriginal) => {
-  const actual = await importOriginal();
+  const actual = await importOriginal<typeof import('react-router-dom')>();
   return {
     ...actual,
     useSearchParams: vi.fn(),
@@ -18,7 +18,7 @@ describe('Pagination Component', () => {
   const mockSetSearchParams = vi.fn();
 
   beforeEach(() => {
-    (useSearchParams as vi.Mock).mockReturnValue([
+    vi.mocked(useSearchParams).mockReturnValue([
       new URLSearchParams(),
       mockSetSearchParams,
     ]);
